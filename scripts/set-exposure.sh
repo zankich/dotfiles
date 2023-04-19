677acfcdc4efc9ce23589b928903194e8f8860d9
Adrian Zankich <adzankich@gmail.com>
2023-04-19 16:53:20 -0700

    update dotfiles

---
+++ scripts/set-exposure.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+set -eu -o pipefail
+
+main() {
+  local exposure
+  exposure="${1}"
+  shift
+
+  v4l2-ctl -d0 -c auto_exposure=1
+  v4l2-ctl -d0 -c exposure_time_absolute="${exposure}00"
+  v4l2-ctl -d0 -c focus_automatic_continuous=0
+}
+
+main "${@}"
---
+++ scripts/set-theme.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#
+set -eu
+
+reload_nvim() {
+  pushd $HOME/.cache/nvim/listen >/dev/null
+  for file in *.pipe; do
+    nvim --server "${file}" --remote-send '<space>r'
+  done
+  popd >/dev/null
+}
+
+set_theme() {
+  local theme
+  theme="${1}"
+  shift
+
+  if [[ "${theme}" == "dark" ]]; then
+    theme="base16_onedark"
+  else
+    theme="base16_equilibrium-gray-light"
+  fi
+
+  tmux new-window -a "zsh -i -c '${theme};omz reload &;exit'"
+}
+
+main() {
+  set_theme "${@}"
+  reload_nvim
+}
+
+main "${@}"
